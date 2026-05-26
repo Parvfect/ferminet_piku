d6a2e2597c3766209478e0a027e2cf44a786d367
Parvfect <parvagrw02@gmail.com>
2026-05-26 12:07:03 +0100

    okay

--- ferminet/configs/silicon/inference.py
+++ ferminet/configs/silicon/inference.py
@@ -120,8 +120,8 @@ if __name__ == '__main__':
 
     #Train
     cfg.optim.iterations = 100000
-    cfg.log.save_freq = 2000
-    cfg.log.save_tfreq = 235
+    cfg.log.save_freq = 2000000
+    cfg.log.save_tfreq = 235000
     cfg.log.restore_from_checkpoint = True
     cfg.log.save_path = "/projects/u6em/parv/silicon/"
     # cfg.debug.deterministic = True  # Use deterministic mode for reproducibility
