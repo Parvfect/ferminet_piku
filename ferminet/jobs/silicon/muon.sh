c48571815c0b4be06f544df510e46f149a42ce20
Parvfect <parvagrw02@gmail.com>
2026-06-10 18:47:27 +0530

    okay

--- ferminet/jobs/silicon/muon.sh
+++ ferminet/jobs/silicon/muon.sh
@@ -24,4 +24,4 @@ IP_ADDR=$(ifconfig 2> /dev/null | awk '$1 == "inet" {print $2}' | head -n 2 | ta
 srun --nodes=8 \
      --gres=gpu:4 \
      --export=ALL \
-     python -u ferminet/configs/pp.py --server_addr="$IP_ADDR:$PORT"
+     python -u ferminet/configs/silicon/pp.py --server_addr="$IP_ADDR:$PORT"
